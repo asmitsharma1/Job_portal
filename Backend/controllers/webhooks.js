1f965a74596dd71c6a20a3c55c3f5d16a13b5cc5
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2025-03-15 13:41:13 +0000

    model

--- Backend/controllers/webhooks.js
+++ Backend/controllers/webhooks.js
@@ -5,7 +5,7 @@ import User from "../models/User.js";
 export const clerkWebhooks = async (req, res) => {
     try{
 
-        // 
+        
         const whook = new webhook (process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verfiy(JSON.stringify(req.body),{
@@ -14,9 +14,47 @@ export const clerkWebhooks = async (req, res) => {
             "svix-signature" : req.headers["svix-signature"]
         })
 
+        const {data, type} = req.body
+
+        switch(type){
+            case 'user.created':{
+
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                    resume: ''
+                }
+                await User.create(userData)
+                res.json({})
+                break;
+            }
+
+            case 'user.updated':{
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id, userData)
+                res.json({})
+                break;
+            }
+
+            case 'user.deleted':{
+                await User.findByIdAndDelete(data.id);
+                res.json({})
+                break;
+            }
+
+            default:
+                break;
+        }
         
 
     }catch(error){
-
+        console.log(error.message)
+        res.json({success:false, message:'webhooks Error'})
     }
 }
