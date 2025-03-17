f82537d9786030672fa4a31ac212898ab21476ed
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2025-03-17 16:10:18 +0000

    cf

---
+++ Backend/controllers/companyController.js
@@ -0,0 +1,43 @@
+
+// register a new company
+export const registerCompany = async (req , res) =>{
+
+}
+
+
+export const loginCompany = async () =>{
+
+}
+
+
+export const getCompanyData = async (req , res) =>{
+
+}
+
+
+export const postJob = async (req , res) =>{
+
+}
+
+
+
+export const getCompanyJobApplications = async (req , res) =>{
+
+}
+
+
+
+export const grtCompanyPostedJobs = async (req , res) =>{
+
+}
+
+
+
+export const changeJobApplicationsStatus = async (req , res) =>{
+
+}
+
+
+export const changeVisibility = async (req , res) =>{
+
+}
